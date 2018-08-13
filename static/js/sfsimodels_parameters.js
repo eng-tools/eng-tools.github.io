8246cf85044f3267863696bb0665f5ba980679fc
millen1m <maxim.millen@gmail.com>
2018-08-13 10:48:33 +0100

    can read csv file of model parameters - tidy.

--- static/js/sfsimodels_parameters.js
+++ static/js/sfsimodels_parameters.js
@@ -15,13 +15,17 @@ function processData(allText) {
   var allTextLines = allText.split(/\r\n|\n/);
   var headers = allTextLines[0].split(',');
   var lines = [];
+  var table = document.getElementById("sfsimodels-parameter-table");
 
-  for (var i = 1; i < allTextLines.length; i++) {
+  for (var i = 0; i < allTextLines.length; i++) {
     var data = allTextLines[i].split(',');
     console.log(data);
     if (data.length == headers.length) {
       var tarr = [];
+      var row = table.insertRow(-1);
+      var cells = [row.insertCell(0), row.insertCell(1), row.insertCell(2)];
       for (var j = 0; j < headers.length; j++) {
+        cells[j].innerHTML = data[j];
         tarr.push(headers[j] + ":" + data[j]);
       }
       lines.push(tarr);
